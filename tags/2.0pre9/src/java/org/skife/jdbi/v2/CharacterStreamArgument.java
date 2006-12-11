76813362935957f07c2a405ae01f3d1c59d8ca7d
brianm <brianm@bef26512-0eee-0310-aad4-d1eb80de6c02>
2006-12-11 22:46:44 +0000

    Tag 2.0pre9

---
+++ tags/2.0pre9/src/java/org/skife/jdbi/v2/CharacterStreamArgument.java
@@ -0,0 +1,27 @@
+package org.skife.jdbi.v2;
+
+import org.skife.jdbi.v2.tweak.Argument;
+
+import java.io.Reader;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ */
+class CharacterStreamArgument implements Argument
+{
+    private final Reader value;
+    private final int length;
+
+    CharacterStreamArgument(Reader value, int length)
+    {
+        this.value = value;
+        this.length = length;
+    }
+
+    public void apply(int position, PreparedStatement statement) throws SQLException
+    {
+        statement.setCharacterStream(position, value, length);
+    }
+}
