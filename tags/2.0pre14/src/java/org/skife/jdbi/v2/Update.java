3921c8c12a5878559c899fc0f0c7bf738e470ac4
brianm <brianm@bef26512-0eee-0310-aad4-d1eb80de6c02>
2006-12-29 01:41:51 +0000

    Tag 2.0pre14

---
+++ tags/2.0pre14/src/java/org/skife/jdbi/v2/Update.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2004-2006 Brian McCallister
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.skife.jdbi.v2;
+
+import org.skife.jdbi.v2.tweak.StatementBuilder;
+import org.skife.jdbi.v2.tweak.StatementLocator;
+import org.skife.jdbi.v2.tweak.StatementRewriter;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Used for INSERT, UPDATE, and DELETE statements
+ */
+public class Update extends SQLStatement<Update>
+{
+    Update(Connection connection, StatementLocator locator, StatementRewriter statementRewriter, StatementBuilder cache, String sql, StatementContext ctx)
+    {
+        super(new Binding(), locator, statementRewriter, connection, cache, sql, ctx);
+    }
+
+    /**
+     * Execute the statement
+     * @return the number of rows modified
+     */
+    public int execute()
+    {
+        return this.internalExecute(QueryPreperator.NO_OP, new QueryResultMunger<Integer>()
+        {
+            public Pair<Integer, ResultSet> munge(Statement results) throws SQLException
+            {
+                return new Pair<Integer, ResultSet>(results.getUpdateCount(), null);
+            }
+        }, QueryPostMungeCleanup.CLOSE_RESOURCES_QUIETLY);
+    }
+}
