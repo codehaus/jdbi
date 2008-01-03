bff83497bb553332d0f5b2f5ae9b307026939476
brianm <brianm@bef26512-0eee-0310-aad4-d1eb80de6c02>
2008-01-03 22:51:15 +0000

    Tag 2.1.0

---
+++ tags/2.1.0/src/java/org/skife/jdbi/v2/logging/NoOpLog.java
@@ -0,0 +1,67 @@
+package org.skife.jdbi.v2.logging;
+
+import org.skife.jdbi.v2.tweak.SQLLog;
+import org.skife.jdbi.v2.Handle;
+
+/**
+ * Default SQLLog implementation, does nothing
+ */
+public final class NoOpLog implements SQLLog
+{
+
+    final static BatchLogger batch = new BatchLogger() {
+
+        public final void add(String sql)
+        {
+        }
+
+        public final void log()
+        {
+        }
+    };
+
+    public void logBeginTransaction(Handle h)
+    {
+    }
+
+    public void logCommitTransaction(Handle h)
+    {
+    }
+
+    public void logRollbackTransaction(Handle h)
+    {
+    }
+
+    public void logObtainHandle(Handle h)
+    {
+    }
+
+    public void logReleaseHandle(Handle h)
+    {
+    }
+
+    public void logSQL(String sql)
+    {
+    }
+
+    public void logPreparedBatch(String sql, int count)
+    {
+    }
+
+    public BatchLogger logBatch()
+    {
+        return batch;
+    }
+
+    public void logCheckpointTransaction(Handle h, String name)
+    {
+    }
+
+    public void logReleaseCheckpointTransaction(Handle h, String name)
+    {
+    }
+
+    public void logRollbackToCheckpoint(Handle h, String checkpointName)
+    {
+    }
+}
---
+++ tags/2.1.0/src/test/org/skife/jdbi/v2/TestColonStatementRewriter.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2004-2007 Brian McCallister
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.skife.jdbi.v2;
+
+import junit.framework.TestCase;
+import org.skife.jdbi.v2.tweak.RewrittenStatement;
+
+import java.util.HashMap;
+
+/**
+ *
+ */
+public class TestColonStatementRewriter extends TestCase
+{
+    private ColonPrefixNamedParamStatementRewriter rw;
+
+    public void setUp() throws Exception
+    {
+        this.rw = new ColonPrefixNamedParamStatementRewriter();
+    }
+
+
+    public void testNewlinesOkay() throws Exception
+    {
+        RewrittenStatement rws = rw.rewrite("select * from something\n where id = :id", new Binding(),
+                                            new StatementContext(new HashMap<String, Object>()));
+        assertEquals("select * from something\n where id = ?", rws.getSql());
+    }
+
+    public void testOddCharacters() throws Exception
+    {
+        RewrittenStatement rws = rw.rewrite(":boo ':nope' _%&^& *@ :id", new Binding(),
+                                            new StatementContext(new HashMap<String, Object>()));
+        assertEquals("? ':nope' _%&^& *@ ?", rws.getSql());
+    }
+
+    public void testNumbers() throws Exception
+    {
+        RewrittenStatement rws = rw.rewrite(":bo0 ':nope' _%&^& *@ :id", new Binding(),
+                                            new StatementContext(new HashMap<String, Object>()));
+        assertEquals("? ':nope' _%&^& *@ ?", rws.getSql());
+    }
+}
