87bb3259189429448d4e3c67055df222aad284b0
brianm <brianm@bef26512-0eee-0310-aad4-d1eb80de6c02>
2009-06-06 05:48:49 +0000

    [maven-release-plugin]  copy for tag jdbi-2.4.4

---
+++ tags/jdbi-2.4.4/src/main/java/org/skife/jdbi/v2/exceptions/StatementException.java
@@ -0,0 +1,47 @@
+package org.skife.jdbi.v2.exceptions;
+
+import org.skife.jdbi.v2.StatementContext;
+
+/**
+ *
+ */
+public abstract class StatementException extends DBIException
+{
+    private final StatementContext statementContext;
+
+    public StatementException(String string, Throwable throwable, StatementContext ctx) {
+        super(string, throwable);
+        this.statementContext = ctx;
+    }
+
+    public StatementException(Throwable cause, StatementContext ctx) {
+        super(cause);
+        this.statementContext = ctx;
+    }
+
+    public StatementException(String message, StatementContext ctx) {
+        super(message);
+        this.statementContext = ctx;
+    }
+
+    public StatementContext getStatementContext() {
+        return statementContext;
+    }
+
+    @Override
+    public String getMessage() {
+        String base = super.getMessage();
+        StatementContext ctx = getStatementContext();
+        if (ctx == null) {
+            return base;
+        }
+        else {
+            return String.format("%s [statement:\"%s\", located:\"%s\", rewritten:\"%s\", arguments:%s]",
+                                 base,
+                                 ctx.getRawSql(),
+                                 ctx.getLocatedSql(),
+                                 ctx.getRewrittenSql(),
+                                 ctx.getBinding());
+        }
+    }
+}
